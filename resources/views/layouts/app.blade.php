a5f21b47f73e997335ca2d39f6c62b37bd352a64
Victor Eduardo <edu.ardo.s.s@hotmail.com>
2019-04-24 23:15:21 -0300

    -a

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Ised') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,23 +22,25 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+     <style>
+     html, body {
+        background-color: #00AD8E;
+        </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+<div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel" >
+            <a class="navbar-brand" href="#">
+
+                     <img src="https://i.imgur.com/oFwLC97.png" width="200"  alt="">
                 </a>
+                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
+                    
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
@@ -69,8 +75,16 @@
                         @endguest
                     </ul>
                 </div>
-            </div>
+            
         </nav>
+       
+       
+  <div class="col-5" style="margin-left:600px "> <img src="storage/I.png" class="float-right" alt="Paris"  width="400" height="200"> </div>
+  <div class="col-5" style="margin-left:520px "> <img src="storage/L.png" class="float-right" alt="Paris"  width="250" height="100"> </div>
+  
+   
+
+        
 
         <main class="py-4">
             @yield('content')
